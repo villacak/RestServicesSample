0be293ea5fc5e12f5ac950414729307f4d3a92b9
villacak <klausvillaca@gmail.com>
2017-12-02 21:46:00 +1100

    Renamed packages; Adding a second server with port 9080 for the UI, still need to test it

---
+++ view/src/main/java/au/com/ui/test/AngularWebApp.java
@@ -0,0 +1,69 @@
+package au.com.ui.test;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.servlet.ServletContainer;
+
+public class AngularWebApp {
+
+  private static final int DEFAULT_PORT = 9080;
+
+  private final String WEB_APP_DIR_LOCATION = "src/app/";
+  private final String ADD_SERVLET = "/*";
+
+  private int serverPort;
+
+  public AngularWebApp(final int serverPort) throws Exception {
+    this.serverPort = serverPort;
+    final Server server = configureServer();
+    server.start();
+    server.join();
+  }
+
+
+
+  /**
+   * Configure the server - Jetty
+   *
+   * @return
+   */
+  private Server configureServer() {
+    final Server server = new Server(serverPort);
+    final ResourceConfig resourceConfig = new ResourceConfig();
+    final ServletContainer servletContainer = new ServletContainer(resourceConfig);
+
+
+    final ServletHolder sh = new ServletHolder(servletContainer);
+    sh.setInitOrder(1);
+
+    WebAppContext webapp = new WebAppContext();
+    webapp.setContextPath(ADD_SERVLET);
+    webapp.setDescriptor(WEB_APP_DIR_LOCATION + "/WEB-INF/web.xml");
+    webapp.setResourceBase(WEB_APP_DIR_LOCATION);
+
+
+    return server;
+  }
+
+
+  /**
+   * Start the server with
+   *
+   * @param args
+   * @throws Exception
+   */
+  public static void main(String[] args) throws Exception {
+    int serverPort = DEFAULT_PORT;
+    if (args.length >= 1) {
+      try {
+        serverPort = Integer.parseInt(args[0]);
+      } catch (NumberFormatException e) {
+        e.printStackTrace();
+      }
+    }
+
+    new AngularWebApp(serverPort);
+  }
+}
